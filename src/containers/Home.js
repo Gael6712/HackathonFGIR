b7bc9ae1964ce303ded236c4ce28ee4a3949dd9b
RafaelDubois <rafaeldubois974@gmail.com>
2021-11-25 15:45:57 +0100

    Ajout hangman

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Hangman from "../components/Hangman/Hangman";
 
 export default function Home() {
   return (
     <div>
       <h1> T'es pendu la Morue !</h1>
+      <Hangman />
     </div>
   );
 }
