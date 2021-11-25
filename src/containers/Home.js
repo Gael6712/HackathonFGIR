4fdf670ac9e153a9c7e0a17e529c3223e144853b
FabriceSerapion <fabrice-serapion_student2021@wilder.school>
2021-11-25 13:53:04 +0100

    ajout lecteur

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Lecteur from "./components/Lecteur";
 
 export default function Home() {
   return (
     <div>
       <h1> T'es pendu la Morue !</h1>
+      <Lecteur />
     </div>
   );
 }
