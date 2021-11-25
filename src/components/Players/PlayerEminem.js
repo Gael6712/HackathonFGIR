d7ad6222f7c20148835340e5691f78a18ebdc6c6
FabriceSerapion <fabrice-serapion_student2021@wilder.school>
2021-11-25 22:41:28 +0100

    fonction cacher le player après six essais

--- src/components/Players/PlayerEminem.js
+++ src/components/Players/PlayerEminem.js
@@ -6,6 +6,13 @@ export default function PlayersEminem() {
   const { dataEminem } = useContext(ApiMusicContext);
   const playEminem = dataEminem.preview;
   console.log(playEminem);
+  const [player, setPlayer] = useState(0);
+
+  const handleSetPlayer = () => {
+    setPlayer(player + 1);
+  };
+
+  console.log(player);
 
   useEffect(() => {
     fetch(
@@ -20,10 +27,12 @@ export default function PlayersEminem() {
     <>
       <figure>
         <h1>{details.name}</h1>
-        <audio controls src={playEminem}>
-          Your browser does not support the
-          <code>audio</code> element.
-        </audio>
+        {player < 6 ? (
+          <audio onPlay={handleSetPlayer} controls src={playEminem}>
+            Your browser does not support the
+            <code>audio</code> element.
+          </audio>
+        ) : <p>Enough plays ! Time to give an answer !</p>}
       </figure>
     </>
   );
